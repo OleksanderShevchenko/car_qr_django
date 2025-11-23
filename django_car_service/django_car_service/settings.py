0b9d63cb7628101f0e588b5cc80d0b2ef3e79e38
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-11-23 14:17:47 +0200

    Update settings to use env variables

--- django_car_service/django_car_service/settings.py
+++ django_car_service/django_car_service/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5u@wyhu=hi)x%ki8w$jx#^5*mj!50d=r)3hp(+#!xjv16)yvvp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
+# DEBUG має бути False, якщо змінної немає, або якщо вона не 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
